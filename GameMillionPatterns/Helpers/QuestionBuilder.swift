589b1134e80d18afa0c5cfd93c507d93a97d0abb
Igor Polousov <igorpolousov@gmail.com>
2022-03-15 12:28:48 +0300

    + helpers

---
+++ GameMillionPatterns/Helpers/QuestionBuilder.swift
@@ -0,0 +1,38 @@
+//
+//  QuestionBuilder.swift
+//  GameMillionPatterns
+//
+//  Created by Igor Polousov on 15.03.2022.
+//
+
+class QuestionsBuilder {
+    private(set) var question: String = ""
+    private(set) var answerOptions: [String] = [String]()
+    private(set) var rightAnswer: Int = 0
+    
+    func build() -> Question {
+        let mixedQuestions = answerOptions.shuffled()
+        for (index, answer) in mixedQuestions.enumerated() {
+            if answer == answerOptions[rightAnswer] {
+                rightAnswer = index
+            }
+        }
+        return Question(question: question, answers: mixedQuestions, correctAnswer: rightAnswer)
+    }
+    
+    func setQuestion(_ question: String) -> Self {
+        self.question = question
+        return self
+    }
+
+    func addAnswers(_ answers: [String]) -> Self {
+        answerOptions = answers
+        return self
+    }
+    
+    func setRightAnswer(_ index: Int) -> Self {
+        rightAnswer = index
+        return self
+    }
+}
+
